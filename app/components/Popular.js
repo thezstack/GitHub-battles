1e7ffb44eaed9ea287a94ccd9cea19ee98a07b0d
Zubair Mughal <mghlzbr@gmail.com>
2018-01-17 10:16:17 -0600

    added Popular component

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -6,19 +6,26 @@ class Popular extends React.Component {
     this.state = {
       selectedLanguage:'All'
     };
+    this.updateLanguage = this.updateLanguage.bind(this);
   }
-
+updateLanguage(lang) {
+  this.setState(function() {
+    return {
+      selectedLanguage: lang
+    }
+  });
+}
 
 
   render () {
     var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
     return (
-
+      
       <ul className='languages'>
         {languages.map(function (lang) {
           return <li key={lang}> {lang} </li>
 
-        })}
+        }, this)}
       </ul>
     )
   }
